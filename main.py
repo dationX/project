00b035947ba6a4bea16bece85cee2553b5b842de
Anton Ushakov <114620658+dationX@users.noreply.github.com>
2025-03-09 17:31:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import telebot
 
+token = "YOUR TOKEN"
+bot = telebot.Telebot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message)
+  bot.send_message()
+
+bot.infinity_polling()
