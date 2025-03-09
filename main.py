34a3627d640c0fc1a10385fb6db1e73c6894874d
Anton Ushakov <114620658+dationX@users.noreply.github.com>
2025-03-09 18:40:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import telebot
+from telebot import types
 
-token = "YOUR TOKEN"
-bot = telebot.Telebot(token)
+bot = telebot.TeleBot("YOUR TOKEN")
+
+names_button = ["Обработать твои привычки", "Рассчитать углеродный след", "Предоставить советы"]
 
 @bot.message_handler(commands=['start'])
-def start_message(message)
-  bot.send_message()
+def send_welcome(message):
+    bot.reply_to(message,
+"""
+Привет! Я Telegram бот, который поможет тебе меньше выделять углекислый газ.
+Если ты хочешь узнать, что я умею, то пропиши: /button.
+""")
 
-bot.infinity_polling()
+@bot.message_handler(commands="button")
+def func_keyboard(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    for name_button in names_button:
+        markup.add(name_button)
+
+    bot.send_message(message.chat.id, "Что я умею: ", reply_markup=markup)
+    
+bot.polling()
